d2e2b4f111b0663b4b06be54236432ea7e18d6a6
Rofak <rofakvkm@gmail.com>
2023-12-20 00:42:29 +0700

    chore: add depandancy to deploy to github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/eslint-module", "@nuxt-alt/http", "@nuxt/ui"],
+  ssr: false,
+  app: {
+    baseURL: "/nuxt-github-pages/",
+    buildAssetsDir: "assets",
+  },
 });
