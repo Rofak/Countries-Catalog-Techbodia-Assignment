98b7cbba9bd7897a06b30a834866155f08d8dfba
Rofak <rofakvkm@gmail.com>
2023-12-19 20:21:15 +0700

    chore: add nuxt-ui dependancy

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div></div>
+  <div>
+    <UAlert
+      description="You can add components to your app using the cli."
+      :avatar="{ src: 'https://avatars.githubusercontent.com/u/739984?v=4' }"
+      title="Heads up!"
+    />
+  </div>
 </template>
